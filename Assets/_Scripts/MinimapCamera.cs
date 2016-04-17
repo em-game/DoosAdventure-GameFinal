9e0c79856e7886e33fc5d5d443179251027667df
orangeem <orangeem@naver.com>
2016-04-17 04:07:08 -0400

    Adding minimap and edting instruction
    bronze coin animation, fixing icebar, adding boyfriend anim

---
+++ Assets/_Scripts/MinimapCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour {
+	public Camera miniCamera;
+
+	private bool _cheatKey;
+
+	// Use this for initialization
+	void Start () {
+		miniCamera.enabled = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Submit"))
+		{
+			this._cheatKey = !this._cheatKey;
+		}
+
+		if (this._cheatKey) {
+			miniCamera.enabled = true;			
+		}
+
+		if (!this._cheatKey) {
+			miniCamera.enabled = false;
+		}
+	
+	}
+}
